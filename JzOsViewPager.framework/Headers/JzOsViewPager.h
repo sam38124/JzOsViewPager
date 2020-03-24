c40513ca58da137087940977cae05651c72ec852
sam38124 <sam38124@gmail.com>
2020-03-24 08:46:17 -0700

    1.0

---
+++ JzOsViewPager.framework/Headers/JzOsViewPager.h
@@ -0,0 +1,19 @@
+//
+//  JzOsViewPager.h
+//  JzOsViewPager
+//
+//  Created by Jianzhi.wang on 2020/3/21.
+//  Copyright © 2020 Jianzhi.wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JzOsViewPager.
+FOUNDATION_EXPORT double JzOsViewPagerVersionNumber;
+
+//! Project version string for JzOsViewPager.
+FOUNDATION_EXPORT const unsigned char JzOsViewPagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JzOsViewPager/PublicHeader.h>
+
+
